d84b9331cd4f7b0a95a61e0805b460d02d7764ab
camposleidiane <leidiane.campos@zup.com.br>
2021-05-31 10:41:16 -0300

    [00] Setup do Projeto

---
+++ src/main/java/zupacademy/leidiane/transacao/DesafioTransacaoOrangeTalentsApplication.java
@@ -0,0 +1,13 @@
+package zupacademy.leidiane.transacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTransacaoOrangeTalentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioTransacaoOrangeTalentsApplication.class, args);
+	}
+
+}
